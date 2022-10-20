29c0185506ff8a5b1bf530ac02b0c30a6a527424
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-20 13:24:26 -0300

    Create aula03-type_void.ts

---
+++ src/aula03-type_void/aula03-type_void.ts
@@ -0,0 +1,17 @@
+function semRetorno(...args: string[]): void {
+    console.log(args.join(' '));
+}
+
+const pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Marcelo',
+
+    exibirNome(): void {
+        console.log(this.nome + '' + this.sobrenome);
+    }
+};
+
+semRetorno('Lucas', 'Marcelo');
+pessoa.exibirNome();
+
+export { pessoa };
