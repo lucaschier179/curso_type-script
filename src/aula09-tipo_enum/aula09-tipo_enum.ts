08fc52c25240f8fe498a689a5de66ca561754f6b
lucaschier <ens-lucasschier@ugv.edu.br>
2022-10-21 12:33:09 -0300

    aula 278 - Tipo Enum (21/10)

---
+++ src/aula09-tipo_enum/aula09-tipo_enum.ts
@@ -0,0 +1,18 @@
+enum Cores{
+    AMARELO, // = 0
+    AZUL, // = 1
+    VERDE, // = 2
+    VERMELHO, // = 3
+}
+
+//console.log(Cores);
+console.log(Cores.AZUL);
+console.log(Cores[0]);
+
+/*
+function escolhaACor(cor: Cores): void{
+    console.log(Cores[cor]);
+}
+
+escolhaACor(Cores.VERDE);
+*/
